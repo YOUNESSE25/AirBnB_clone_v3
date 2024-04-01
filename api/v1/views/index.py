0dca6fba6fdcc4eb12c55eb0c29c8055eb506c44
YOUNESSE25 <younessehaouas@gmail.com>
2024-04-01 07:40:31 +0200

    v3

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,31 @@
 """create a file index.py"""
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status')
 def GetStatus():
     """returns a JSON: "status": "OK"/"""
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats', strict_slashes=False)
+def Getstats():
+    """"""
+    objects = {
+        'amenities': Amenity,
+        'cities': City,
+        'places': Place,
+        'reviews': Review,
+        'states': State,
+        'users': User
+    }
+    for Key, Val in objects.items():
+        objects[Key] = storage.count(Val)
+    return jsonify(objects)
