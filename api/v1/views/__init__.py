693b76928b565d505c8fac2c70330461bb274671
YOUNESSE25 <younessehaouas@gmail.com>
2024-03-31 18:19:02 -0700

    v3

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 """create a file __init__.py"""
 from flask import Blueprint
-from api.v1.views.index import *
+
+
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+
+from api.v1.views.index import *
