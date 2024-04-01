c10476ea9c3df4c9ea910412390974040b05b8aa
YOUNESSE25 <younessehaouas@gmail.com>
2024-04-01 08:24:44 +0200

    v3

--- api/v1/app.py
+++ api/v1/app.py
@@ -3,7 +3,7 @@
 """
 from models import storage
 from api.v1.views import app_views
-from flask import Flask
+from flask import Flask, jsonify
 from os import environ
 
 
@@ -17,6 +17,12 @@ def closedb(error):
     storage.close()
 
 
+@app.errorhandler(404)
+def pageNotFound(error):
+    """"""
+    return jsonify({"error": "Not found"}), 404
+
+
 if __name__ == "__main__":
     """main"""
     HOST = environ.get('HBNB_API_HOST')
@@ -25,4 +31,4 @@ if __name__ == "__main__":
     PORT = environ.get('HBNB_API_PORT')
     if not PORT:
         port = '5000'
-    app.run(host=HOST, port=PORT, threaded=True)
+    app.run(host=HOST, port=PORT, debug=True, threaded=True)
