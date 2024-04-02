66ec7e00d2979a2dd459237012ccea358fda7a15
YOUNESSE25 <younessehaouas@gmail.com>
2024-04-02 02:13:02 +0200

    v3

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,7 +2,7 @@
 """"""
 from models import storage
 from api.v1.views import app_views
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response
 from os import environ
 
 
@@ -19,7 +19,7 @@ def closedb(error):
 @app.errorhandler(404)
 def not_found(error):
     """"""
-    return jsonify({'error': "Not found"}), 404
+    return make_response(jsonify({'error': "Not found"}), 404)
 
 
 if __name__ == "__main__":
